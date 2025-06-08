e19e49473f0b6b633e6d4d01690b926f9e3ef1ab
6enard <109796647+6enard@users.noreply.github.com>
2025-06-08 21:36:25 +0300

    updated products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
   return (
     <div>
       {/* Hero Section */}
-      <section className="bg-gradient-to-r from-orange-700 to-orange-500 text-white py-16" >
+      <section className="bg-gradient-to-r from-blue-700 to-orange-500 text-white py-16" >
         <div className="container mx-auto px-4" >
           <div className="max-w-3xl mx-auto text-center" >
             <h1 className="text-4xl md:text-5xl font-bold mb-6 animate-fade-in-up">
